826b6cab928ea4e9a961b3ed90a6633d91491604
manuelmasia <manuelmasia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-17 05:39:31 +0000

    v.1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/pixgridder/trunk@757480 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pixgridder.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+Plugin Name: PixGridder
+Plugin URI: http://www.pixedelic.com/plugins/pixgridder
+Description: A simple page composer that splits your pages/posts into grid with columns and rows
+Version: 1.0.0
+Author: Manuel Masia | Pixedelic.com
+Author URI: http://www.pixedelic.com
+License: GPL2
+*/
+
+define( 'PIXGRIDDER_PATH', plugin_dir_path( __FILE__ ) );
+define( 'PIXGRIDDER_URL', plugin_dir_url( __FILE__ ) );
+
+require_once( PIXGRIDDER_PATH . 'lib/functions.php' );
+
+register_activation_hook( __FILE__, array( 'PixGridder', 'activate' ) );
+register_uninstall_hook( __FILE__, array( 'PixGridder', 'uninstall' ) );
+
+PixGridder::get_instance();
