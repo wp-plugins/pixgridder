2fc829c5dcf34b2d58936e52906f305fdfc66f80
manuelmasia <manuelmasia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-17 05:38:16 +0000

    v.1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/pixgridder/trunk@757479 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ scripts/pixgridder_tinyMCE.js
@@ -0,0 +1,55 @@
+jQuery.noConflict();
+
+function pixgridderTinyMCEinit() {
+	var DOM = tinymce.DOM;
+
+	tinymce.init;
+
+	tinymce.create('tinymce.plugins.PixGridder', {
+		mceTout : 0,
+
+		init : function(ed, url) {
+			var t = this;
+
+			ed.onBeforeSetContent.add(function(ed, o) {
+
+				var head = tinyMCE.activeEditor.dom.select('head');
+
+				var thisLink = jQuery("<link>"),
+					thisLink2 = jQuery("<link>");
+				thisLink.attr({
+						type: 'text/css',
+						rel: 'stylesheet',
+						href: pixgridder_url+'/css/tinymce_frame.css'
+				});
+				jQuery(head).append( thisLink );
+
+				if (pix_builder_modal=='open') {
+
+					setTimeout(function(){
+						var h = (jQuery('#textarea_builder').height() - (jQuery('#textArea_toolbargroup').height() + jQuery('#wp-textArea-editor-tools').height())),
+							h2 = (jQuery('#textarea_builder').height() - (jQuery('#qt_textArea_toolbar').height() + jQuery('#wp-textArea-editor-tools').height()));
+
+						ed.theme.resizeTo('auto', (h-42));
+						jQuery('#wp-textArea-editor-container textarea').css({height:(h2-20)});
+
+						jQuery(window).bind('resize',function(){
+							h = (jQuery('#textarea_builder').height() - (jQuery('#textArea_toolbargroup').height() + jQuery('#wp-textArea-editor-tools').height()));
+							h2 = (jQuery('#textarea_builder').height() - (jQuery('#qt_textArea_toolbar').height() + jQuery('#wp-textArea-editor-tools').height()));
+
+							ed.theme.resizeTo('auto', (h-42));
+							jQuery('#wp-textArea-editor-container textarea').css({height:(h2-20)});
+						});
+					},10);
+				}
+
+			});
+
+		}
+
+	});
+
+	tinymce.PluginManager.add('pixgridder', tinymce.plugins.PixGridder);
+
+}
+jQuery(function() { pixgridderTinyMCEinit(); });
